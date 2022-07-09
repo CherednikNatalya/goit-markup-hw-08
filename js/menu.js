ff0d940b9e22ae9184c0cb0c036922988f1c7a95
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-07-09 21:52:35 +0300

    vjl

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
+
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(".header__button"),
-    closeMenuBtn: document.querySelector(".mob__button"),
-    menu: document.querySelector(".mob-menu"),
-    body: document.querySelector("body"),
+    openMenuBtn: document.querySelector('.header__button'),
+    closeMenuBtn: document.querySelector('.mob__button'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
